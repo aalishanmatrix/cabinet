d7ed2dc711d3b145e94d0f8e2c673c60eec37b33
Aidan Follestad <drummer.aidan@gmail.com>
2013-08-03 17:50:24 -0500

    Updates.

--- src/com/afollestad/cabinet/fragments/prefs/RootFragment.java
+++ src/com/afollestad/cabinet/fragments/prefs/RootFragment.java
@@ -1,8 +1,11 @@
 package com.afollestad.cabinet.fragments.prefs;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 import com.afollestad.cabinet.R;
+import org.sufficientlysecure.rootcommands.RootCommands;
 
 /**
  * @author Aidan Follestad (afollestad)
@@ -13,5 +16,17 @@ public class RootFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefs_root);
+
+        findPreference("root_enabled").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                Boolean value = (Boolean) newValue;
+                if (value && !RootCommands.rootAccessGiven()) {
+                    Toast.makeText(getActivity(), R.string.root_denied, Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                return true;
+            }
+        });
     }
 }
