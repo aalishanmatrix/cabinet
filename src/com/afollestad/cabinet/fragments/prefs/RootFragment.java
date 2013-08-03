5325d92e06f4940f3af61cc8cd43d44d44b074a8
Aidan Follestad <drummer.aidan@gmail.com>
2013-08-03 17:55:36 -0500

    Updates

--- src/com/afollestad/cabinet/fragments/prefs/RootFragment.java
+++ src/com/afollestad/cabinet/fragments/prefs/RootFragment.java
@@ -1,9 +1,12 @@
 package com.afollestad.cabinet.fragments.prefs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 import com.afollestad.cabinet.R;
+import com.afollestad.cabinet.ui.MainActivity;
 import org.sufficientlysecure.rootcommands.RootCommands;
 
 /**
@@ -20,8 +23,12 @@ public class RootFragment extends PreferenceFragment {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
                 Boolean value = (Boolean) newValue;
-                if (value && !RootCommands.rootAccessGiven())
+                if (value && !RootCommands.rootAccessGiven()) {
+                    Toast.makeText(getActivity(), R.string.root_denied, Toast.LENGTH_SHORT).show();
                     return false;
+                }
+                getActivity().startActivity(new Intent(getActivity(), MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                getActivity().finish();
                 return true;
             }
         });
