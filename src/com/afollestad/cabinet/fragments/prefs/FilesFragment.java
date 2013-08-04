0da1aa3b82b10bed83e02f4467782b9b27a3d697
Aidan Follestad <drummer.aidan@gmail.com>
2013-08-04 14:43:50 -0500

    Added sort setting

--- src/com/afollestad/cabinet/fragments/prefs/FilesFragment.java
+++ src/com/afollestad/cabinet/fragments/prefs/FilesFragment.java
@@ -1,6 +1,7 @@
 package com.afollestad.cabinet.fragments.prefs;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import com.afollestad.cabinet.R;
 
@@ -13,5 +14,23 @@ public class FilesFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefs_files);
+
+        findPreference("file_sorting").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                switch (Integer.parseInt(newValue.toString())) {
+                    default:
+                        preference.setSummary(R.string.sorting_folderfile);
+                        break;
+                    case 1:
+                        preference.setSummary(R.string.sorting_alphabetical);
+                        break;
+                    case 2:
+                        preference.setSummary(R.string.sorting_extension);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
