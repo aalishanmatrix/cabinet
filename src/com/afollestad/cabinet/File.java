8e9b4c54832dfd9ad307c9be9c36601e02ebb066
Aidan Follestad <drummer.aidan@gmail.com>
2013-08-04 14:47:21 -0500

    Added sort setting

--- src/com/afollestad/cabinet/File.java
+++ src/com/afollestad/cabinet/File.java
@@ -183,7 +183,17 @@ public class File extends java.io.File implements SilkComparable<File> {
     public static class ExtensionComparator implements java.util.Comparator<File> {
         @Override
         public int compare(File lhs, File rhs) {
-            return lhs.getExtension().compareTo(rhs.getExtension());
+            // First, folders always come before files
+            if (lhs.isDirectory() && !rhs.isDirectory()) {
+                return -1;
+            } else if (lhs.isDirectory() && rhs.isDirectory()) {
+                return lhs.getName().compareTo(rhs.getName());
+            } else if (!lhs.isDirectory() && rhs.isDirectory()) {
+                return 1;
+            } else {
+                // Once folders are sorted, sort files by extension
+                return lhs.getExtension().compareTo(rhs.getExtension());
+            }
         }
     }
 
